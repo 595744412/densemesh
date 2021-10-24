6218c7b6a853cb762a1027dc36ab012c3a4808bc
595744412 <595744412@qq.com>
2021-10-24 14:09:08 +0800

    init

---
+++ src/camera.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#ifndef CAMERA_H
+#define CAMERA_H
+
+namespace densemesh
+{
+
+    class Camera
+    {
+    public:
+        Camera(int a);
+        void print();
+
+    private:
+    };
+}
+
+#endif
